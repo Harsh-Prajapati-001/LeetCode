20a966afbc0baa8801c1b732d0e88ea2ee182669
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-07-27 01:54:30 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome.java
+++ two-pointers/valid-palindrome.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() -1;
-        while(left<right){
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
-                left++;
+        int j = 0;
+        int k = s.length() -1;
+        while(j<k){
+            while(j<k && !Character.isLetterOrDigit(s.charAt(j))){
+                j++;
             }
-            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-                right--;
+            while(j<k && !Character.isLetterOrDigit(s.charAt(k))){
+                k--;
             }
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+            if(Character.toLowerCase(s.charAt(j)) != Character.toLowerCase(s.charAt(k))){
                  return false;
             }
-            left++;
-            right--;
+            j++;
+            k--;
         }
         return true;
     }
