03603c1d7ebc7e5c19e801f3e12fcf2041bcb169
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-07-29 10:39:12 +0530

    LC-long-pressed-name [Runtime: N/A; Memory: N/A]

--- string/long-pressed-name.java
+++ string/long-pressed-name.java
@@ -4,12 +4,13 @@ class Solution {
         int m = typed.length();
         int i =0;
         int j =0;
-        while(j<m && i<n){
-            if(name.charAt(i) == typed.charAt(j)){
+        while(j<m ){
+            if(i<n && name.charAt(i) == typed.charAt(j)){
                 i++;j++;
-            }else if(name.charAt(i) != typed.charAt(j)){
+            }else if( j>0 && (typed.charAt(j) ==typed.charAt(j-1))){
                 j++;
             }
+            else{return false;}
 
         }
         return i==n;
