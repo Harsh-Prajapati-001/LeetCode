8503befd93c3fafa21502fa1802b20e6f5871ed8
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-03 22:18:06 +0530

    LC-relative-sort-array [Runtime: N/A; Memory: N/A]

---
+++ bubble-sort/relative-sort-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int n = arr2.length;
+        int m = arr1.length;
+        int i = 0;
+        int k =0;
+        while(i<n){
+            for (int j =0; j<m; j++){
+                if(arr1[j] ==arr2[i]){
+                    int temp = arr1[k];
+                    arr1[k] = arr1[j];
+                    arr1[j] = temp;
+                    k++;
+                }
+            }
+            i++;
+        }
+        ArrayList<Integer> rest = new ArrayList<>();
+        int z = k;
+        while(k<m){
+            rest.add(arr1[k]);
+            k++;
+        }
+        Collections.sort(rest);
+        for(int x =0; x<rest.size();x++){
+            arr1[z] = rest.get(x);
+            z++;
+        }
+        return arr1;
+        
+    }
+}
