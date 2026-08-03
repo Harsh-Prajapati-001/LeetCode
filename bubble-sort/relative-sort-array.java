729e7c14b195adcb77a932ce2383dbd03ca90937
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-03 23:04:01 +0530

    LC-relative-sort-array [Runtime: N/A; Memory: N/A]

--- bubble-sort/relative-sort-array.java
+++ bubble-sort/relative-sort-array.java
@@ -1,32 +1,24 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int n = arr2.length;
-        int m = arr1.length;
-        int i = 0;
-        int k =0;
-        while(i<n){
-            for (int j =k; j<m; j++){
-                if(arr1[j] ==arr2[i]){
-                    int temp = arr1[k];
-                    arr1[k] = arr1[j];
-                    arr1[j] = temp;
-                    k++;
-                }
+        int[] freq = new int[1001];
+        for(int n :arr1){
+            freq[n]++;
+        }
+        int[] result = new int[arr1.length];
+        int idx =0;
+        for(int i : arr2){
+            while(freq[i]>0){
+                result[idx] = i;
+                idx++;
+                freq[i]--;
             }
-            i++;
         }
-        ArrayList<Integer> rest = new ArrayList<>();
-        int z = k;
-        while(k<m){
-            rest.add(arr1[k]);
-            k++;
-        }
-        Collections.sort(rest);
-        for(int x =0; x<rest.size();x++){
-            arr1[z] = rest.get(x);
-            z++;
-        }
-        return arr1;
-        
+        for(int j =0; j<1000; j++){
+            while(freq[j]>0){
+                result[idx++] =j;
+                freq[j]--;
+            }
+        }        
+       return result; 
     }
 }
