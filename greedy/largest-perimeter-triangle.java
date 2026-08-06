81da4847160acec2090e024612553be5f69b9b01
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-06 21:38:09 +0530

    LC-largest-perimeter-triangle [Runtime: N/A; Memory: N/A]

---
+++ greedy/largest-perimeter-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        int n = nums.length;
+        int i =1;
+        int maxi = 0;
+        while( i<n-1){
+            if(nums[i-1] >= nums[i] + nums[i+1]){
+             i++;
+            } else if(nums[i] >= nums[i-1] +nums[i+1]){
+                i++;
+            } else if(nums[i+1] >= nums[i] +nums[i-1]){
+                i++;
+            }else{
+                maxi = Math.max(maxi ,  nums[i] + nums[i-1] +nums[i+1]);
+                i++;
+            }
+        }
+        return maxi;
+    }
+}
