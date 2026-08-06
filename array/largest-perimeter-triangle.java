d3eb0b3812e7a4dbef4740044c45b111a166147a
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-06 21:46:45 +0530

    LC-largest-perimeter-triangle [Runtime: N/A; Memory: N/A]

--- array/largest-perimeter-triangle.java
+++ array/largest-perimeter-triangle.java
@@ -3,6 +3,10 @@ class Solution {
         int n = nums.length;
         int i =1;
         int maxi = 0;
+        if(n<3){
+            return 0;
+        }
+        Arrays.sort(nums);
         while( i<n-1){
             if(nums[i-1] >= nums[i] + nums[i+1]){
              i++;
