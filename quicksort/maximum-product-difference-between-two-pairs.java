1206e1e095fdf42667b474e59d75eed49516ca7a
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-06 22:31:27 +0530

    LC-maximum-product-difference-between-two-pairs [Runtime: N/A; Memory: N/A]

--- quicksort/maximum-product-difference-between-two-pairs.java
+++ quicksort/maximum-product-difference-between-two-pairs.java
@@ -1,13 +1,32 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
         int n = nums.length; 
+        int u1 =0; int u2 = 0;
+        int l1 = Integer.MAX_VALUE;
+        int l2 = Integer.MAX_VALUE;
         int maxi =0;
-        if(n<4){
-            return maxi;
+        for(int x:nums){
+            if(x<l2){
+                if(x<l1){ 
+                    l2 = l1;
+                    l1 = x;
+                }
+                else{
+                    l2 =x;
+                }
+            }
+            if(x>u2){
+                if(x>u1){
+                    u2=u1;
+                    u1 = x;
+                }
+                else{
+                    u2 = x;
+                }
+            }
         }
-        Arrays.sort(nums);
     
-        maxi = (nums[n-1]*nums[n-2])-(nums[0]*nums[1]);
+        maxi = (u1*u2)-l1*l2;
        return maxi;
     }
 }
