3f92cdf0fb48900094d76de51044a6484f5aafbb
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-07 13:58:26 +0530

    LC-h-index [Runtime: N/A; Memory: N/A]

--- counting-sort/h-index.java
+++ counting-sort/h-index.java
@@ -3,8 +3,11 @@ class Solution {
         int n = citations.length;
         int max = 0;
         Arrays.sort(citations);
-        if(n <=1 && citations[0] >1 ){
-            return 0;
+        if(n <=1 && citations[0] >=1 ){
+            return 1;
+        }
+        if(n<= 1){
+
         }
         for(int i =0; i<n;i++){
             int x = citations[i];
