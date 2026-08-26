10dc77af50cbf1e7ac0426becb4542f8e4f4ffda
Harsh Prajapati <harshprajapati0756@gmail.com>
2026-08-27 00:16:43 +0530

    LC-shortest-and-lexicographically-smallest-beautiful-string [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n = s.length();
+
+        int left = 0;
+        int ones = 0;
+
+        String ans = "";
+
+        for (int right = 0; right < n; right++) {
+            if (s.charAt(right) == '1') {
+                ones++;
+            }
+
+            // We have exactly k ones
+            while (ones == k) {
+
+                // Remove leading zeroes
+                while (left <= right && s.charAt(left) == '0') {
+                    left++;
+                }
+
+                String current = s.substring(left, right + 1);
+
+                if (ans.isEmpty()
+                        || current.length() < ans.length()
+                        || (current.length() == ans.length()
+                            && current.compareTo(ans) < 0)) {
+                    ans = current;
+                }
+
+                // Move left past the first '1'
+                if (s.charAt(left) == '1') {
+                    ones--;
+                }
+
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
